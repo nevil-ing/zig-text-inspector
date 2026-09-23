bbd0da8745b0f78d0d46cd13c28d8742489044ff
Nevil-ing <nevilpurpp12@gmail.com>
2026-09-23 11:42:54 +0300

    refactored the code to use functions and fixed the counters

--- src/main.zig
+++ src/main.zig
@@ -7,8 +7,7 @@ const zig_text_inspector = @import("zig_text_inspector");
 pub fn main() !void {
     const text: []const u8 = "Zig is simple, fast and explicit. Learning Zig is fun";
     const vowel_c = vowel_count(text);
-    var space_count: usize = 0;
-
+    const space_count = countSpaces(text);
     const word = "Zig";
     const zig_c = zig_count(text, word);
     const word_count = countWords(text);
@@ -18,32 +17,28 @@ pub fn main() !void {
     //flag to track when inside words.
 
     //print text len.
-    std.debug.print("'{s}' this text has  {d} characters\n", .{ text, text.len });
-
-    var byte_array: [text.len]u8 = undefined;
-    @memcpy(&byte_array, text);
+    std.debug.print("'{s}' this text has  {d} bytes\n", .{ text, text.len });
 
     //get the first 3 bytes and last 4 bytes
-    const first_bytes = byte_array[0..3];
-    const text_len = byte_array.len;
-    const last_bytes = byte_array[text_len - 4 .. text_len];
+    //
+    const first_bytes = text[0..3];
+    const text_len = text.len;
+    const last_bytes = text[text_len - 4 .. text_len];
     //print
-    std.debug.print("First 3 bytes: {s}\n", .{first_bytes});
+    std.debug.print("First 3 bytes: {s} \n", .{first_bytes});
+    for (first_bytes) |byte| {
+        std.debug.print("{x:0>2}", .{byte});
+    }
+    std.debug.print("\n", .{});
     std.debug.print("Last 4 bytes: {s}\n", .{last_bytes});
+    for (last_bytes) |byte| {
+        std.debug.print("{x:0>2}", .{byte});
+    }
+    std.debug.print("\n", .{});
 
     //check if "zig" and "fun" exists.
     std.debug.print("Text starts with Zig: {any}\n", .{std.mem.startsWith(u8, text, "Zig")});
     std.debug.print("Text Ends with fun: {any}\n", .{std.mem.endsWith(u8, text, "fun")});
-
-    //loop
-    for (text) |char| {
-        switch (char) {
-            ' ' => {
-                space_count += 1;
-            },
-            else => {},
-        }
-    }
 
     std.debug.print("word_count:{d}\n", .{word_count});
     std.debug.print("vowel_count:{d}\n", .{vowel_c});
@@ -51,6 +46,19 @@ pub fn main() !void {
     std.debug.print("Zig_count:{d}\n", .{zig_c});
 }
 
+//fn to count space_count
+fn countSpaces(text: []const u8) usize {
+    var count_sp: usize = 0;
+    for (text) |char| {
+        switch (char) {
+            ' ' => {
+                count_sp += 1;
+            },
+            else => {},
+        }
+    }
+    return count_sp;
+}
 //function to count words.
 fn countWords(text: []const u8) usize {
     var count: usize = 0;
